2e649889664c8e0b51ff773b5de3af9296a901d4
andrew <andrew.chebli@outlook.com>
2023-02-06 13:15:56 -0500

    adding files for the pages

--- app/my_app/src/App.js
+++ app/my_app/src/App.js
@@ -3,20 +3,20 @@ import ResponsiveHeader from "./Components/header";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import  Dashboard  from "./pages/Dashboard";
-// import SignInPage from "./pages/SignInPage";
+ import SignInPage from "./pages/SignInPage";
 // import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div className="App">
       <ResponsiveHeader></ResponsiveHeader>
-      {/* <BrowserRouter>
+      {/* { <BrowserRouter>
         <Routes>
-          { <Route path="/Dashboard" element={<Dashboard />} /> }
-          { <Route path="/SignIn" element={<SignInPage />} /> }
-          { <Route path="/SignUp" element={<SignUpPage />} /> }
+          { { <Route path="/Dashboard" element={<Dashboard />} /> } }
+          { { <Route path="/SignIn" element={<SignInPage />} /> } }
+          { { <Route path="/SignUp" element={<SignUpPage />} /> } }
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter> } */}
     </div>
   );
 }
