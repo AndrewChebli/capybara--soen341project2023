57a705bd22127a3afd75e53865b171dd8d1778b4
andrew <andrew.chebli@outlook.com>
2023-02-27 18:13:36 -0500

    Adding logic for email checking,formatting code

--- react-project/src/components/SignIn.js
+++ react-project/src/components/SignIn.js
@@ -1,27 +1,34 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { Visibility, VisibilityOff } from "@material-ui/icons";
+// import {InputAdornment,IconButton,} from "@material-ui/core";
 
-function Copyright(props) {
+function Copyright(props) { 
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -29,13 +36,45 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignIn() {
+  const [values, setValues] = React.useState({
+    email: "",
+    password: "",
+    showPassword: false,
+  });
+  const [emailError, setEmailError] = React.useState(false);
+  const [emailHelperText, setEmailHelperText] = React.useState("");
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
+  };
+
+  const validateEmail = (email) => {
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return regex.test(email);
+  };
+  const handleEmailBlur = (event) => {
+    const email = event.target.value;
+    const isValid = validateEmail(email);
+    if (!isValid) {
+      setEmailError(true);
+      setEmailHelperText("Please enter a valid email address");
+    } else {
+      setEmailError(false);
+      setEmailHelperText("");
+    }
   };
 
   return (
@@ -45,24 +84,34 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
               fullWidth
               id="email"
               label="Email Address"
+              value={values.email}
+              onChange={handleChange("email")}
+              onBlur={handleEmailBlur}
+              error={emailError}
+              helperText={emailHelperText}
               name="email"
               autoComplete="email"
               autoFocus
@@ -77,6 +126,7 @@ export default function SignIn() {
               id="password"
               autoComplete="current-password"
             />
+            
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
@@ -86,17 +136,22 @@ export default function SignIn() {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              disabled={!validateEmail(values.email)}
             >
               Sign In
             </Button>
-            <Grid container sx={{justifyContent:'center'}}>
+            <Grid container sx={{ justifyContent: "center" }}>
               {/* <Grid item xs>
                 <Link href="#" variant="body2">
                   Forgot password?
                 </Link>
               </Grid> */}
               <Grid item>
-                <Link href="../SignUpPage" variant="body2" sx={{justifyContent:'center'}}>
+                <Link
+                  href="../SignUpPage"
+                  variant="body2"
+                  sx={{ justifyContent: "center" }}
+                >
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
