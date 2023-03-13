e56a040ae4fdece43777626a729e5b5e4d08c5f7
andrew <andrew.chebli@outlook.com>
2023-03-12 21:22:04 -0400

    adding the company route

---
+++ server/routes/company-routes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const companyController = require("../controllers/company-controllers");
+
+router.get("/getall", (req, res, next) => {
+  console.log("GET request to /company/getall");
+  companyController.getAllCompanies(req, res, next);
+});
+
+router.get("/getone/:_id", (req, res, next) => {
+  console.log("GET request to /company/getone/:_id");
+  companyController.getCompanyById(req, res, next);
+});
+
+router.delete("/:_id", (req, res, next) => {
+  console.log("DELETE request to /company/remove/:_id");
+  companyController.removeCompany(req, res, next);
+});
+
+router.patch("/:_id", (req, res, next) => {
+  console.log("PATCH request to /company/update/:_id");
+  companyController.updateCompany(req, res, next);
+});
+
+router.post("/", (req, res, next) => {
+  console.log("POST request to /company/register");
+  companyController.addCompany(req, res, next);
+});
+
+module.exports = router;
+
+
