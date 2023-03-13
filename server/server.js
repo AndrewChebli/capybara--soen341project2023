05c2b8c626f5a2ea1b8b36b74d25b8541baccbad
andrew <andrew.chebli@outlook.com>
2023-03-12 21:24:30 -0400

    removing the routes and replacing with routers

--- server/server.js
+++ server/server.js
@@ -1,19 +1,50 @@
-//=======================Dependencies==========================
+//=======================Imports==========================
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const UserRegisterModel = require("./models/registerUser.model.js");
-const AddJobModel = require("./models/addJob.model.js");
-const UserUpdateModel = require("./models/updateUser.model.js");
-const UserLoginModel = require("./models/loginUser.model.js");
+const bodyParser = require("body-parser");
+const {check , validationResult} = require('express-validator');
+const HttpError = require("./models/http-error");
+
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
 app.use(cors());
+app.use(bodyParser.json());
+//======================LOCAL IMPORTS======================
+
+
+//=========================ROUTES==========================
+
+const employeeRoutes = require("./routes/employee-routes");
+const jobRoutes = require("./routes/job-routes");
+const companyRoutes = require("./routes/company-routes");
+
+app.use("/api/employee", employeeRoutes);
+app.use("/api/company", companyRoutes);
+app.use("/api/job", jobRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
 
 app.use(express.json());
+
+app.use((error, req, res, next) => {
+
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+
+});
+
 let db;
 
 //============================================================
@@ -27,170 +58,6 @@ mongoose.connection.once("open", () => {
 });
 //===================END OF CONNECTION BLOCK===================
 
-//=======================ENDPOINTS=============================
-//
-//=====================REGISTER ENDPOINT=======================
-
-app.post("/register", async (req, res) => {
-  console.log("register attempt from " + req.body.email);
-  console.log("BODY  : " + JSON.stringify(req.body));
-
-  const { firstName, lastName, email, password } = req.body;
-  const { dateStartedSchool, dateCompletedSchool, school, academicProgram } =
-    req.body;
-  const {
-    dateStartedWork,
-    dateCompletedWork,
-    companyName,
-    jobTitle,
-    Description,
-  } = req.body;
-  const { resume } = req.body;
-
-  console.log("lastName " + lastName);
-  console.log("RESUME " + resume);
-
-  try {
-    await UserRegisterModel.create({
-      firstname: firstName,
-      lastname: lastName,
-      email: email,
-      password: password,
-      resume: resume,
-      education: {
-        Start: dateStartedSchool,
-        End: dateCompletedSchool,
-        School: school,
-        Degree: academicProgram,
-      },
-      previousExperience: [
-        {
-          Start: dateStartedWork,
-          End: dateCompletedWork,
-          Company: companyName,
-          Position: jobTitle,
-          Description: Description,
-        },
-      ],
-    });
-  } catch (error) {
-    console.log(error);
-  }
-  res.json({ status: "success" });
-});
-
-//=====================LOGIN ENDPOINT=======================
-app.post("/login", async (req, res) => {
-  console.log("login attempt from " + req.body.email);
-  console.log("BODY  : " + JSON.stringify(req.body));
-
-  const { email, password } = req.body;
-  const database_response = await UserLoginModel.findOne(
-    {
-      email: email,
-      password: password,
-    },
-    {}
-  );
-  console.log(JSON.stringify(database_response));
-  console.log("database_response : " + database_response);
-  if (database_response) {
-    res.send({ status: "success", data: database_response });
-  } else {
-    res.send({ status: "failed" });
-  }
-});
-
-
-
-
-//=====================UPDATE ENDPOINT=======================
-app.post("/update", async (req, res) => {
-  console.log("update attempt from " + req.body.email);
-  console.log("BODY  : " + JSON.stringify(req.body));
-  const { firstName, lastName, email, password } = req.body;
-  const { dateStartedSchool, dateCompletedSchool, school, academicProgram } = req.body;
-  const { dateStartedWork, dateCompletedWork, companyName, jobTitle, Description } = req.body;
-  const {_id} = req.body;
-  const {resume, resuneName} = req.body;
-  console.log(_id);
-  
-  let response_from_database = await UserUpdateModel.findOneAndUpdate({email},
-        {
-          firstname: firstName,
-          lastname: lastName,
-          email: email,
-          password: password,
-          resume: resume,
-          resuneName: resuneName,
-          education: {
-            Start: dateStartedSchool,
-            End: dateCompletedSchool,
-            School: school,
-            Degree: academicProgram,
-          },
-          previousExperience: [
-            {
-              Start: dateStartedWork,
-              End: dateCompletedWork,
-              Company: companyName,
-              Position: jobTitle,
-              Description: Description,
-            },
-          ],
-        },
-        { new: true } //new true is to return the updated document
-      );
-  console.log("response_from_database : " + response_from_database);
-  if (response_from_database) {
-    res.send({ status: "success", db_response: response_from_database });
-  }else{
-    res.send({ status: "failed" });
-  }
-
-
-
-});
-
-// ========== GET JOBS =========
-
-app.post("/getAllJobs", async (req, res) => {
-  const all_jobs = await AddJobModel.find({});
-  console.log("from mongo all_jobs : " + all_jobs);
-
-  res.send({ status: "success", data: all_jobs });
-
-});
-
-// =========================== ADD JOB =========================
-
-app.post("/addJob", async (req, res) => {
-  console.log("add job attemmpt " + req.body.email);
-  console.log("BODY  : " + JSON.stringify(req.body));
-  const {jobTitle, companyName, deadlineDay, deadlineMonth, deadlineYear, jobDescription, jobRequirements, jobSalary, jobBenefits} = req.body;
-  try {
-    await AddJobModel.create({     // .create sends the creation of the document described below to the database
-    title: jobTitle,
-    company: companyName,
-    description: jobDescription,
-    Dday:  deadlineDay,
-    Dmonth: deadlineMonth,
-    Dyear: deadlineYear,
-    salary: jobSalary,
-    benefits: jobBenefits,
-    requirements: jobRequirements,
-
-      
-    });
-  } catch (error) {
-    console.log(error);
-  }
-  res.json({ status: "success" });
-});
-
-
-
-//=====================END OF ENDPOINTS=======================
 //====================LISTENING PORT==========================
 app.listen(8080, () => {
   console.log("server started on port 8080");
