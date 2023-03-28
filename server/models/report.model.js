13fde19bf68dfe80f64564d9e483f81215a4ada1
SamuelLopezF <43799537+SamuelLopezF@users.noreply.github.com>
2023-03-28 17:01:03 -0400

    added the model for reports, new routes new controllers and a new page for reports

---
+++ server/models/report.model.js
@@ -0,0 +1,19 @@
+const mongoose = require ("mogoose");
+
+const ReportModel = new mongoose.Schema(
+  {
+    type : {type: String, required: true},
+    message : {type : String, required: true},
+    reason : {type : String, required: true},
+    whistleblower_id : {type : String, required: true},
+    whilestblower_name : {type : String, required: true},
+    offender_id : {type : String, required: true},
+    offender_name : {type : String, required: true},
+    data : {type : Object, required: true}
+  },{collection: "reports"}
+  );
+
+const Report = mongoose.model("Report", ReportModel, "reports");
+
+
+module.exports = Report;
