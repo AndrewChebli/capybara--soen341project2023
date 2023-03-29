067b44fd233d94d51d27a3349e42244e115f2a6b
SamueLopezF <Samuellopez.f@gmail.com>
2023-03-28 20:23:41 -0400

    fixed typos

--- client/src/components/JobPostingSummary.js
+++ client/src/components/JobPostingSummary.js
@@ -16,43 +16,10 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-function stringToColor(string) {
-  // assigns a color to the icon of a job posting card.
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = "#";
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
 
-function stringAvatar(name) {
-  if (name.split(" ").length === 1) {
-    return {
-      sx: {
-        bgcolor: stringToColor(name),
-      },
-      children: `${name[0]}`,
-    };
-  } else {
-    return {
-      sx: {
-        bgcolor: stringToColor(name),
-      },
-      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
-    };
-  }
-}
+
 
 function JobPostingSummary(props) {
   const { data, handleLinkChange } = props;
@@ -276,5 +243,6 @@ function JobPostingSummary(props) {
     </div>
   );
 }
+
 
 export default JobPostingSummary;
