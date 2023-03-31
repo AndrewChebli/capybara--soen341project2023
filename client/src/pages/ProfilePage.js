3130d3ff61f80c817b86cef6f7874940f779747f
SamuelLopezF <43799537+SamuelLopezF@users.noreply.github.com>
2023-03-31 14:16:16 -0400

    modified the profile page

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -2,10 +2,16 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
-import ProfileSidebar from "../components/ProfileSidebar";
 import ResumeViewer from "../components/ResumeViewer";
 import Grid from "@mui/material/Grid";
 import { useState, useEffect } from "react";
+import Avatar from "@mui/material/Avatar";
+import { Card, CardContent, CardMedia } from "@mui/material";
+import Chip from "@mui/material/Chip";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneEnabledIcon from "@mui/icons-material/PhoneEnabled";
+import SchoolIcon from "@mui/icons-material/School";
+import Paper from "@mui/material/Paper";
 
 function ProfilePage() {
   const [employeeInfo, setEmployeeInfo] = useState({
@@ -50,82 +56,120 @@ function ProfilePage() {
   }, [setEmployeeInfo]);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginTop: 15,
-        marginBottom: 8,
-      }}
-    >
-      <ProfileSidebar user={employeeInfo} />
+    <Box>
+      <Box sx={{ mt: 15, maxWidth: " 80%", pl: "10%" }}>
+        <Grid container direction="row ">
+          <Grid item container direction=" column " xs={8}>
+            <Grid item>
+              <Card sx={{ borderRadius: 5 }}>
+                <CardMedia sx={{ height: 305, mt: 5 }}>
+                  <img
+                    style={{ borderRadius: 20 }}
+                    alt="Remy Sharp"
+                    src="https://picsum.photos/900/300"
+                  />
+                  <Box
+                    sx={{
+                      position: "relative",
+                      top: -100,
+                      left: 70,
+                    }}
+                  >
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="https://picsum.photos/100/100"
+                      sx={{
+                        width: 100,
+                        height: 100,
+                        border: 4,
+                        borderColor: "white",
+                      }}
+                    />
+                  </Box>
+                </CardMedia>
+                <CardContent sx={{ ml: 5 }}>
+                  <Typography variant="h2" component="div" gutterBottom>
+                    {employeeInfo.firstName} {employeeInfo.lastName}
+                  </Typography>
+                  <Grid
+                    container
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="center"
+                    spacing={2}
+                    sx={{ mt: 2 }}
+                  >
+                    <Grid item xs={3}>
+                      <Chip
+                        icon={<PhoneEnabledIcon fontSize="small" />}
+                        label={"514-123-4567"}
+                      />
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Chip
+                        icon={<EmailIcon fontSize="small" />}
+                        label={employeeInfo.email}
+                      />
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Chip
+                        icon={<SchoolIcon fontSize="small" />}
+                        label={"Bachelor's in " + employeeInfo.education.degree}
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Paper
+                      sx={{
+                        p: 2,
+                        flexGrow: 1,
+                        borderRadius: 5,
+                      }}
+                    >
+                      <Typography variant="h6" sx={{ textAlign: "left" }}>
+                        I'm a software engineer with over five years of
+                        experience in developing and maintaining complex
+                        software systems. My expertise lies in developing
+                        scalable, maintainable, and secure applications using
+                        programming languages such as Java, Python, and
+                        JavaScript. I have a deep understanding of software
+                        design patterns, algorithms, and data structures, which
+                        allows me to write efficient and robust code.
+                      </Typography>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Divider sx = {{ my : 2 }} />
+                    <Paper
+                      sx={{
+                        p: 2,
+                        flexGrow: 1,
+                        borderRadius: 5,
+                      }}
+                    >
+                      <Typography variant="h4" sx={{ textAlign: "left" }}>
+                        {employeeInfo.experience[0].position}
 
-      <Grid container sx={{ ml: 20 }} maxWidth="70%" spacing={3}>
-        <Grid item xs={12}>
-          <Box sx={{ fontSize: 30 , mr: 50, mb: 4}} >
-            <Typography variant="h1" color="primary" fontFamily={"Roboto"}>
-              Your Profile
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={6} md={6}>
-          <Typography variant="h6">Personal Info</Typography>
-          <Divider variant="middle" sx={{ mb: 2 }} />
-          <Typography id = "profileName" variant="body1" sx={{ marginBottom: 2 }}>
-            {employeeInfo.firstName} {employeeInfo.lastName}
-          </Typography>
-          <Typography variant="body1" sx={{ marginBottom: 2 }}>
-            {employeeInfo.email}
-          </Typography>
-        </Grid>
-        <Grid item xs={6} md={6}>
-          <Grid item xs={3} md={6}>
-            <Typography variant="h6">Education</Typography>
-            <Divider variant="middle" sx={{ mb: 2 }} />
-          </Grid>
-          <Grid item xs={3} md={6}>
-            <Typography variant="body1" sx={{ marginBottom: 2 }}>
-              {employeeInfo.education.school} {employeeInfo.education.degree}
-            </Typography>
-            <Typography variant="body1" sx={{ marginBottom: 2 }}>
-              {employeeInfo.education.start} {employeeInfo.education.end}
-            </Typography>
+                        </Typography>
+                        <Typography variant="h5" sx={{ textAlign: "left" }}>
+                        At {employeeInfo.experience[0].company}
+                        
+                        </Typography>
+                        <Typography variant="h6" sx={{ textAlign: "left" }}>
+                        {employeeInfo.experience[0].description}
+                        
+                        </Typography>
+                        <Typography variant="h6" sx={{ textAlign: "left" }} >
+                        {"from " } {employeeInfo.experience[0].start} {" to "}{employeeInfo.experience[0].end}
+                        </Typography>
+                    </Paper>
+                  </Grid>
+                </CardContent>
+              </Card>
+            </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={6} md={6}>
-          <Typography variant="h6">Work Experience</Typography>
-          <Divider variant="middle" sx={{ mb: 2 }} />
-          <Typography variant="body1" sx={{ mb: 2, mt: 2 }}>
-            {employeeInfo.experience[0].company}{" "}
-            {employeeInfo.experience[0].position}
-          </Typography>
-          <Typography variant="body1" sx={{ marginBottom: 2 }}>
-            {employeeInfo.experience[0].start} {employeeInfo.experience[0].end}
-          </Typography>
-          <Typography variant="body1" sx={{ marginBottom: 2 }}>
-            {employeeInfo.experience[0].description}
-          </Typography>
-        </Grid>
-        <Grid item xs={3} md={6}>
-          <Grid item xs={3} md={6}>
-            <Typography variant="h6">Skills</Typography>
-          </Grid>
-          <Grid item xs={3} md={6}>
-            <Divider variant="middle" sx={{ mb: 2 }} />
-            <Typography variant="body1" sx={{ marginBottom: 2 }}>
-              React, Node.js, JavaScript
-            </Typography>
-          </Grid>
-        </Grid>
-      </Grid>
-
-      <Typography variant="h6" sx={{ fontWeight: "bold", mt: 10 }}>
-        Resume
-        <Divider variant="middle" sx={{ mb: 2 }} />
-      </Typography>
-
-      <ResumeViewer></ResumeViewer>
+      </Box>
     </Box>
   );
 }
